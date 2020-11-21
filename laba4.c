44ffbe0784f28716ace926f8f3f1f867caffb665
YushkevichV89 <60590091+YushkevichV89@users.noreply.github.com>
2020-11-21 08:37:21 +0700

    Update laba4.c

--- laba4.c
+++ laba4.c
@@ -1,6 +1,8 @@
 #include <sys/io.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>           
+#include <sys/stat.h>  
 #include <sys/types.h>
 #include <stdio.h>
 #include <semaphore.h>
@@ -8,43 +10,48 @@
 
 
 FILE *fd;
-sem_t semaphore;
-
+sem_t *semaphore1;
+sem_t *semaphore2;
 
 void* square(void* args) {
     int i, res=0;
     for (i = 0; i < 100; i++) {
-      sem_wait(&semaphore);
+      sem_wait(semaphore2);
       res=i*i;
       fprintf(fd,"step %d: calculate %d*%d=%d \n", i, i, i, res);
       res=0;
-      sem_post(&semaphore);
+      sem_post(semaphore1);
     }
 }
 
 void* cube(void* args) {
   int i, res=0;
   for (i = 0; i < 100; i++) {
-    sem_wait(&semaphore);
+    sem_wait(semaphore1);
     res=i*i*i;
     fprintf(fd,"step %d: calculate %d*%d*%d=%d \n", i, i, i, i, res);
     res=0;
-    sem_post(&semaphore);
+    sem_post(semaphore2);
   }
 }
 
 int main(int argc, char const *argv[]) {
-	  fd = fopen("log.txt", "w");
+      fd = fopen("log.txt", "w");
       pthread_t thread1;
       pthread_t thread2;
 
-      sem_init(&semaphore, 0, 1);
+      semaphore1 = sem_open("/my_named_semaphore1", O_CREAT, S_IRUSR | S_IWUSR, 0);
+      semaphore2 = sem_open("/my_named_semaphore2", O_CREAT, S_IRUSR | S_IWUSR, 1);
 
       pthread_create(&thread1, NULL, square, NULL);
       pthread_create(&thread2, NULL, cube, NULL);
 
       pthread_join(thread1, NULL);
       pthread_join(thread2, NULL);
+      
+      
+      sem_unlink("/my_named_semaphore1");
+      sem_unlink("/my_named_semaphore2");
       printf("done \n");
       return 0;
 }
